f3b02a6884531501e97442b33c2d18b3c548636e
Steelcrow64 <salvo.cleto@gmail.com>
2024-07-06 16:39:29 +0200

    Add tutorial screen and human/robot models

--- Assets/Script/GameOver.cs
+++ Assets/Script/GameOver.cs
@@ -8,4 +8,10 @@ public class GameOver : MonoBehaviour
         if (other.CompareTag("Player"))
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.R))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
